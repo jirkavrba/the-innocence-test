9390a83b3da9e4924953ed16a26202893ae089c2
Jiří Vrba <jiri@vrba.dev>
2022-09-13 09:14:31 +0200

    Basic questions component design

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -7,9 +7,21 @@ interface QuestionProps {
 }
 
 const Question: FC<QuestionProps> = ({question, setAnswer}: QuestionProps) => {
+    const toggle = () => setAnswer(!question.answer);
+    const color = question.answer 
+        ? "border-blue-500 text-blue-900"
+        : "border-gray-200"
+
+
     return (
-        <figure className="">
-            <figcaption>{ question.text }</figcaption>
+        <figure className={`${color} select-none border-2 font-bold p-5 rounded-xl flex flex-row items-center justify-between cursor-pointer transition hover:shadow-lg`} onClick={() => toggle()}>
+            <figcaption>
+                { question.text }
+            </figcaption>
+
+            <div className={`transition transform ${question.answer ? "scale-150" : ""} ml-5`}>
+                { question.emoji }
+            </div>
         </figure>
     );
 };
