379e26ec2c24ecd64202aa6cdc727afa837e0587
Jiří Vrba <jiri@vrba.dev>
2024-02-25 21:02:01 +0100

    Fix indexing answers in the result grid

--- src/components/ResultsGrid.tsx
+++ src/components/ResultsGrid.tsx
@@ -10,8 +10,8 @@ const ResultsGrid: FC<ResultsGridProps> = ({ answers }) => {
         <div className="flex flex-col items-center justify-center mt-10">
             <div className="grid grid-cols-10 gap-3 lg:gap-5 bg-neutral-900 p-4 rounded-2xl">
                 {
-                    answers.map(answer => {
-                        return <div key={answer.index} className={`text-xl ${answer.answer ? "" : "opacity-25 filter grayscale"}`}>{answer.emoji}</div>;
+                    answers.map((answer, index) => {
+                        return <div key={index} className={`text-xl ${answer.answer ? "" : "opacity-25 filter grayscale"}`}>{answer.emoji}</div>;
                     })
                 }
             </div>
